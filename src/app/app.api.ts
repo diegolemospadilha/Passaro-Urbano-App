efdc78725a3a87c8913a5c557e226402f0b17581
lemospadilha <dlemos.padilha@gmail.com>
2019-07-04 20:10:14 -0300

    Alter to const of API

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,4 +1,4 @@
-export const URL_API_OFERTAS = 'http://localhost:3000/ofertas'
-export const URL_API_COMO_USAR = 'http://localhost:3000/como-usar'
-export const URL_API_ONDE_FICA = 'http://localhost:3000/onde-fica'
-export const URL_API_PEDIDOS = 'http://localhost:3000/pedidos'
+export const URL_API_OFERTAS = 'https://my-json-server.typicode.com/diegolemospadilha/Passaro-Urbano-App/ofertas'
+export const URL_API_COMO_USAR = 'https://my-json-server.typicode.com/diegolemospadilha/Passaro-Urbano-App/como-usar'
+export const URL_API_ONDE_FICA = 'https://my-json-server.typicode.com/diegolemospadilha/Passaro-Urbano-App/onde-fica'
+export const URL_API_PEDIDOS = 'https://my-json-server.typicode.com/diegolemospadilha/Passaro-Urbano-App/pedidos'
