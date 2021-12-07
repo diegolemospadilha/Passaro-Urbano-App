d716bdbdc07e15ee99a3b7042d1cabb7651f651e
lemospadilha <dlemos.padilha@gmail.com>
2021-12-06 21:57:23 -0300

    style: adicionado ajustes css nas páginas de contato e sobre

--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -11,7 +11,7 @@ export class ContatoComponent implements OnInit {
   public formulario: FormGroup = new FormGroup({
     'nome' : new FormControl(null, [ Validators.required, Validators.minLength(5), Validators.maxLength(30)]),
     'email' : new FormControl(null, [ Validators.required, Validators.minLength(1), Validators.maxLength(50)]),
-    'menssagem' : new FormControl(null),
+    'mensagem' : new FormControl(null),
   })
 
    enviado: boolean = false;
